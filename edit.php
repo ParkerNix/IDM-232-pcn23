c546ac3f263458f3bacb15b3132be9dbe83bd3c0
Parker Nix <71665527+ParkerNix@users.noreply.github.com>
2021-10-30 04:03:55 -0400

    Fixing tablet sizes, ignore config

---
+++ edit.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel= "Normalizer" href= "css/normalizer.css"/>
+    <link rel= "stylesheet" href= "css/main.css"/>
+    <title>Home | Recipe & Me</title>
+</head>
+<body>
+<?php include 'includes/header.php' ?>
+    <div class="body">
+        <h1 class="header">Edit Recipe</h1>
+        <form class="editing">
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" placeholder= "Title...">
+            </div>
+            <div class="description">
+                <h2>Description</h2>
+                <textarea class="type" rows="5" cols="60" name="description" placeholder= "This dish is..."></textarea>
+            </div>
+            <div class="ingredients">
+                <h2>ingredients</h2>
+                <textarea class="type" rows="5" cols="60" name="ingredients" placeholder= "1 cup of..."></textarea>
+            </div>
+            <div class="steps">
+                <h2>Steps</h2>
+                <textarea class="type" rows="5" cols="60" name="steps" placeholder="First..."></textarea>
+            </div>
+            <div class="upload">
+                <h2>Upload Image</h2>
+                <input type="file">
+            </div>
+            <input type="submit" value="Submit" class="submit">
+        </form>
+        <button class="submit" id="delete">Delete</button>
+    </div>
+<?php include 'includes/footer.php' ?>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel= "Normalizer" href= "css/normalizer.css"/>
+    <link rel= "stylesheet" href= "css/main.css"/>
+    <title>Home | Recipe & Me</title>
+</head>
+<body>
+<?php include 'includes/header.php' ?>
+    <div class="body">
+        <div class="feature">
+            <div class="featimg">
+                <a href="details.php"><img src="images/grilledcheese.jpg" alt="grilled cheese"></a>
+            </div>
+            <div class="feattext">
+                <h1>Ooey-Gooey Grilled Cheese</h1>
+                <p>Looking for a dish you can make for lunch that won’t 
+                    take hours and doesn’t cost you an arm and a leg? This dish has you covered.</p>
+                <a href="details.php"><p>Go to recipe ></p></a>
+            </div>
+        </div>
+        <div class="menu">
+            <div class="categories">
+                <h2>Categories</h2>
+                <a href="categories.php"><img src="images/categories.png" alt="categories"></a>
+            </div>
+            <div class="all">
+                <h2>All Recipes</h2>
+                <a href="all.php"><img src="images/all.png" alt="all recipes"></a>
+            </div>
+            <div class="random">
+                <h2>Random Recipe</h2>
+                <a href="#"><img src="images/random.png" alt="random"></a>
+            </div>
+        </div>
+    </div>
+<?php include 'includes/footer.php' ?>
+</body>
+</html>
